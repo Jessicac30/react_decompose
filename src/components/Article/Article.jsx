7b76eaebacd233904a51c8a799a79d55c0893eca
Jessica Carvalho <jmaria.carvalho30@gmail.com>
2024-11-19 14:56:16 -0300

    solution

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,5 +1,3 @@
-// import a css file containig article styles
+import './Article.css';
 
-// Create an Article function returning the HTML of article block
-
-// Add a default export statement for Article component to use it in the other files
+export const Article = () => <Article>Page Article</Article>;
